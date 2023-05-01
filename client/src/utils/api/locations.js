95117be24fa02e766b4ee07fbe1e18d25ea19804
Jose Del Valle <jdelvalle88@live.com>
2023-05-01 18:27:46 -0400

    another build

--- client/src/utils/api/locations.js
+++ client/src/utils/api/locations.js
@@ -1,20 +1,15 @@
 // pages/api/locations.js
-import axiosInstance from '../api/axiosInstance';
+import axios from 'axios';
 
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    const { q } = req.query;
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${q}.json?access_token=${pk.eyJ1IjoiamRlbHZhbGxlMTIiLCJhIjoiY2xnc2YyZG92MW50MTNqbWs1enV6a3gyOSJ9.NyHRU66sRujiLrEwi7AXow}`;
-
-    axiosInstance.get('../locations')
-      .then(response => {
-        res.status(200).json(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-      });
-  } else {
-    res.status(405).end();
+export default async function handler(req, res) {
+  const { q } = req.query;
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${q}.json?access_token=${pk.eyJ1IjoiamRlbHZhbGxlMTIiLCJhIjoiY2xnc2YyZG92MW50MTNqbWs1enV6a3gyOSJ9.NyHRU66sRujiLrEwi7AXow}`;
+  
+  try {
+    const response = await axios.get(url);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
   }
 }
